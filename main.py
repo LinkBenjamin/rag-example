47c56d0cc49b0715df876ac153a44ec4bfb20a2e
Ben Link <link.ben@gmail.com>
2024-07-30 18:34:59 -0400

    updates prior to builders day

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
 from langchain_community.llms import Ollama
-from dotenv import load_dotenv
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -10,22 +8,9 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from tubescriber import YouTubeTranscriber
     
-MODEL_ID = 'llama3'
+MODEL_ID = 'llama3.1'
 BASE_URL = 'http://127.0.0.1:11434'
-VIDEO_ID = 'B9-3duh8-TA'
-
-def invocations(retrieval_chain):
-    response1 = retrieval_chain.invoke({"input": "Create a summary of this message that's less than 800 characters long.  Then add several hashtags that would be appropriate if this were the youtube description of the video, in order to maximize its social media reach."})
-
-    print(f"{response1['answer']}\n")
-
-    response2 = retrieval_chain.invoke({"input": "Create a clickbait style title for the message based on its overall theme."})
-
-    print(f"{response2['answer']}\n")
-
-    response3 = retrieval_chain.invoke({"input": "Locate at least 3 potential quotable snippets within the message that could make good short-form video content.  Provide ONLY the snippets, do not explain why you selected them."})
-
-    print(f"{response3['answer']}\n")
+VIDEO_ID = 'zjkBMFhNj_g'
 
 def main():
     video_id = VIDEO_ID
@@ -63,6 +48,11 @@ def make_vector_store_retriever(result):
         base_url=BASE_URL
     )
     return Chroma.from_texts(chunks, embed_model).as_retriever()
+
+def invocations(retrieval_chain):
+    response1 = retrieval_chain.invoke({"input": "Create an outline and a summary of this message for someone who wants to know the main points but only has a few minutes to read it."})
+
+    print(f"{response1['answer']}\n")
 
 if __name__ == "__main__":
     main()
